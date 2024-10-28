14007d769aff7481b34bfd1758dd42c67382da37
Saydullayev017 <saydullayevjavlonbek2002@gmail.com>
2024-10-28 20:05:20 +0500

    добавил в директорию assets 5 фотографии для карточек. и внедрил PHP код [200~ронумерованные от 1 до 5. тк.

--- index.php
+++ index.php
@@ -31,13 +31,18 @@
         <h3 class="mb-5">Наши статьи</h3>
 
         <div class="d-flex flex-wrap">
-            <!-- Тут цыкл для карточек -->
+            
+        <!-- Тут цыкл для карточек -->
             <?php for($i = 0; $i < 5; $i++): ?>
                 <div class="card mb-4 rounded-3 shadow-sm">
                     <div class="card-header py-3">
                         <h4 class="my-0 fw-normal">Просто текст</h4>
                     </div>
                     <div class="card-body">
+
+                        <!-- добавляю из директории assets фотографии которые пронумерованные от 1 до 5. тк.
+                        Цыкл начинается с 0 до 5 я к $i прибавляю + 1. -->
+                        <img src="assets/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail">
                         <ul class="list-unstyled mt-3 mb-4">
                             <li>10 users included</li>
                             <li>2 GB of storage</li>
@@ -50,6 +55,43 @@
             <?php endfor; ?>
         </div>
     </div>
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        SJ
+        <small class="d-block mb-3 text-body-secondary">© 2017–2024</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
   </div>
 </body>
 </html>
