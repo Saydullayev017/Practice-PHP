1bda79f66f98378c85023493deb977aba094ce21
Saydullayev017 <saydullayevjavlonbek2002@gmail.com>
2024-10-31 05:00:19 +0500

    сделал форму в файле about.php для формы сделал проверка данных и отправку сообщение -  в файле check.php

--- about.php
+++ about.php
@@ -8,10 +8,19 @@
     <title>PHP</title>
 </head>
 <body>
-<div class="container">
-
+  <div class="container">
     <!-- тут я подключаю header  -->
     <?php require "blocks/header.php" ?>
+    
+    <!-- форма -->
+    <div class="container mt-5">
+      <h3>Контактная форма</h3>
+      <form action="check.php" method="post">
+        <input type="email" name="email" placeholder="Ввудите Email" class="form-control"><br>
+        <textarea name="message" class="form-control" placeholder="Сообщение"></textarea><br>
+        <button type="submit" name="send" class="btn btn-success">Отправить</button><br>
+      </form>
+    </div>
 
     <!-- тут я подключаю footer -->
     <?php require "blocks/footer.php" ?>
